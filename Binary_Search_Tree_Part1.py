87076c08267a19bcfa80d4a905e832a44082276d
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-14 11:21:20 +0800

    adding in_order_traversal

--- Binary_Search_Tree_Part1.py
+++ Binary_Search_Tree_Part1.py
@@ -6,3 +6,34 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
+    def add_child(self, data):
+        if data == self.data:
+            return 
+
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visit base node
+        elements.append(self.data)
+        
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
